79df3a530325a03def50daab12257b531be5abaf
TheBoringEdward <querulator78@gmail.com>
2022-12-06 08:19:18 +0100

    Added fib

--- src/de/edward/Main.java
+++ src/de/edward/Main.java
@@ -6,35 +6,55 @@ public class Main {
 
     Scanner scn2 = new Scanner(System.in);
 
-    double potenz(double b, int e){
+    double power(double b, int e){
         if (e > 0){
-            return b * potenz(b,e-1); // This is screwing with my brain.
+            return b * power(b,e-1); // This is screwing with my brain.
         } else {
             return 1;
         }
     }
 
-    /*
-    int knnchn(int e){ // e = number of months
-        if (e > 0){
-
+    int fib(int e){ // e = number of months
+        if (e > 2){
+            return fib(e-1) + fib(e-2);
+            // 01 01 02 03 05 08 13
+            // adds the last two values to receive the new value
         } else {
-            return 1; // Placeholder
+            return 1;
         }
     }
-     */
 
-    void results(){
+    void resultsPower(){
         System.out.println("Please enter the double.");
         double b = scn2.nextDouble();
         System.out.println("Please enter the integer.");
         int e = scn2.nextInt();
-        System.out.println(potenz(b, e));
+        System.out.println(power(b, e));
+    }
+
+    void resultsFib(){
+        System.out.println("Please enter the integer count for months.");
+        int e = scn2.nextInt();
+        System.out.println(fib(e));
+    }
+
+    void option(){
+        System.out.println("Please enter 1 for calculating powers, 2 for knnchn");
+        int e = scn2.nextInt();
+        switch(e){
+            default -> System.out.println("Invalid option");
+            case 1 -> {
+                resultsPower();
+            }
+            case 2 -> {
+                resultsFib();
+            }
+        }
     }
 
     public static void main(String[] args) {
         Main m = new Main();
-        m.results();
-        System.out.print("\n\n ======= This code has been provided by TheBoringEdward =======\n");
+        m.option();
+        System.out.print("\n ======= This code has been provided by TheBoringEdward =======\n");
     }
 }
